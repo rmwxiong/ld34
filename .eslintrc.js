3c8d56b98a03f7daf7d16e45223cb645b0c1256c
Maxwell Xiong <mxiong@uchicago.edu>
2015-12-11 23:29:26 -0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'google',
+  es6: true,
+  env: {
+    browser: true,
+    node: true
+  }
+};
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
+'use strict';
+
 require('../css/main.css');
 
 const WHO = 'JS';
-let greeter = (who) => 'Hello from ' + who + '!';
+let blah = () => {};
+blah();
+let greeter = who => 'Hello from ' + who + '!';
 
 document.getElementById('app').appendChild(
-    document.createTextNode(greeter(WHO))
+  document.createTextNode(greeter(WHO))
 );
