1e9e74db882d4465ff47978e5522cb44cc9ce330
Maxwell Xiong <mxiong@uchicago.edu>
2015-12-13 00:42:36 -0800

    Add audio

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ import {controls} from './controls';
 import DodgeFalling from './games/two/dodge-falling';
 import StayBetween from './games/two/stay-between';
 import Snapshot from './games/one/snapshot';
+import bgm from 'file!../ld34bgm.mp3';
 
 (function() {
   createjs.Ticker.setFPS(60);
@@ -19,9 +20,21 @@ import Snapshot from './games/one/snapshot';
   let score = 0;
   let startTime = Date.now();
 
+  setupSounds();
   startGames();
 
   keyboard.bind('g', startGames);
+  function setupSounds() {
+    let sounds = [{
+      id: 'bgm',
+      src: bgm.substring(1)
+    }];
+    createjs.Sound.addEventListener('fileload', event => {
+      createjs.Sound.play(event.src);
+    });
+    createjs.Sound.registerSounds(sounds, '/');
+  }
+
   function startGames() {
     startTime = Date.now();
     score = 0;
@@ -34,13 +47,13 @@ import Snapshot from './games/one/snapshot';
     let game1spawn = setTimeout(() => {
       let game3 = new Snapshot(['space']);
       games.push(game3);
-    }, 10000);
+    }, 13710);
     gameSpawns.push(game1spawn);
 
     let game2spawn = setTimeout(() => {
       let game2 = new StayBetween(['up', 'down']);
       games.push(game2);
-    }, 20000);
+    }, 27420);
     gameSpawns.push(game2spawn);
   }
 
